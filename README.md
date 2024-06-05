6ffa111a08580393e39c85d2e44c56e2f9e32378
sakis <th.chousiadas@gmail.com>
2024-06-05 18:54:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Security Architecture of JWT Authentication
+A small application demonstrating the architecture of Spring Security using JWT in Bearer Token. The application has built upon Java 17 Spring 6, Spring Boot 3 and Spring Security 6.3
---
+++ src/main/java/gr/aueb/cf/jwtapplication/authentication/RegisterDTO.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.jwtapplication.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterDTO {
+    private String username;
+    private String password;
+    private String email;
+    private String roles;
+}
